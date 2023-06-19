c3924be511b56576243458e4f84b1432cc30c6f9
kshitijdalvi4 <111135860+kshitijdalvi4@users.noreply.github.com>
2023-06-19 17:58:03 +0530

    JS added
    Functionality v1.1

--- JS/site.js
+++ JS/site.js
@@ -1,6 +1,38 @@
 
-//Function Call -> helloWorld
-function helloWorld() {
+function getVal(){
 
-    alert("Hello World");
+
+    document.getElementById("alert_info").classList.add("invisible");
+
+    let userString=document.getElementById("inputString").value;
+
+    let rvrsdString=reverse(userString);
+
+     display(rvrsdString);
+}
+
+
+function reverse(userString){
+    let rvrsdString=[];
+
+    for (let i=userString.length-1 ;i>=0 ;i--) {
+     
+        
+
+        // ** Most Important Line //
+        rvrsdString += userString[i];
+    }
+  
+     return rvrsdString;
+}
+
+
+function display(rvrsdString){
+
+    document.getElementById("msg").innerHTML=`The string reversed is ${rvrsdString}`;
+
+
+    //revome invisibility
+    document.getElementById("alert_info").classList.remove("invisible");
+
 }
